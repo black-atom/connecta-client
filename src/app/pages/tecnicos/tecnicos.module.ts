8a206a131e0dc8c73f34a7da574cd7a4344dd7d8
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-07-26 14:15:21 -0300

    add the template to novo tecnico page

--- src/app/pages/tecnicos/tecnicos.module.ts
+++ src/app/pages/tecnicos/tecnicos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgaModule } from '../../theme/nga.module';
 
 import { TecnicosComponent } from './tecnicos.component';
 import { NovoTecnicoComponent } from './components/novo-tecnico';
@@ -8,7 +9,7 @@ import { GerenciarComponent } from './components/gerenciar';
 import { routing } from './tecnicos.routing';
 
 @NgModule({
-  imports: [CommonModule, routing],
+  imports: [CommonModule, NgaModule, routing],
   declarations: [TecnicosComponent, NovoTecnicoComponent, GerenciarComponent],
 })
 export class TecnicosModule { }

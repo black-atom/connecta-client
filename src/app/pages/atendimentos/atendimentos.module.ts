e3f040c3e130a5166fcc085454328a7c5d0ceaff
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-07-26 15:37:15 -0300

    add the new component detalhes atendimento

--- src/app/pages/atendimentos/atendimentos.module.ts
+++ src/app/pages/atendimentos/atendimentos.module.ts
@@ -6,11 +6,21 @@ import { AtendimentosComponent } from './atendimentos.component';
 import { PerguntasComponent } from './components/perguntas';
 import { NovoAtendimentoComponent } from './components/novo-atendimento';
 import { GerenciarComponent } from './components/gerenciar';
-
+import { DetalhesAtendimentoComponent } from './components/detalhes-atendimento';
 import { routing } from './atendimentos.routing';
 
 @NgModule({
-  imports: [CommonModule, NgaModule, routing],
-  declarations: [AtendimentosComponent, PerguntasComponent, NovoAtendimentoComponent, GerenciarComponent],
+  imports: [
+    CommonModule, 
+    NgaModule,
+     routing
+    ],
+  declarations: [
+    AtendimentosComponent, 
+    PerguntasComponent, 
+    NovoAtendimentoComponent, 
+    GerenciarComponent, 
+    DetalhesAtendimentoComponent
+  ],
 })
 export class AtendimentosModule { }

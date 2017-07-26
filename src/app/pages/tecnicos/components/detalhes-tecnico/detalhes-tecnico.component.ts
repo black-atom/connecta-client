2d2af48b6fc23ce99b9d20ab39e0d86079c3c5c7
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-07-26 15:36:48 -0300

    add the new componenent detalhes tecnico and add the template to gerenciar tecnico page

---
+++ src/app/pages/tecnicos/components/detalhes-tecnico/detalhes-tecnico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-tecnico',
+  templateUrl: './detalhes-tecnico.component.html',
+  styleUrls: ['./detalhes-tecnico.component.scss']
+})
+export class DetalhesTecnicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

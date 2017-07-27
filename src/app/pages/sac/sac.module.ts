656adefdbcd7f2be34af8e6fe6be2abb7e179f9c
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-07-27 11:49:21 -0300

    add the template to consulta page in the sac module

--- src/app/pages/sac/sac.module.ts
+++ src/app/pages/sac/sac.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NgaModule } from '../../theme/nga.module';
 
 import { SacComponent } from './sac.component';
 import { ConsultaComponent } from './components/consulta/consulta.component';
@@ -9,6 +11,8 @@ import { routing } from './sac.routing';
 @NgModule({
   imports: [
     CommonModule,
+    NgaModule,
+    Ng2SmartTableModule,
     routing
   ],
   declarations: [SacComponent, ConsultaComponent]

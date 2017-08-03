993a8f2a475c01e936579373b019a50894995e47
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-08-03 15:05:55 -0300

    continuing the implementation of connect atendimentos page

--- src/app/pages/atendimentos/components/associar/associar.component.ts
+++ src/app/pages/atendimentos/components/associar/associar.component.ts
@@ -1,7 +1,11 @@
+import { AtendimentoModel } from './../../../../models/atendimento/atendimento.interface';
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 import { TecnicoModel } from '../../../../models/tecnico/tecnico.interface';
 import { TECNICOSMOCK } from './../../../../utils/mocks/tecnicos.mock';
+import { AtendimentosDisponiveisComponent } from './atendimentos-disponiveis/atendimentos-disponiveis.component';
+
 
 @Component({
   selector: 'app-associar',
@@ -12,13 +16,20 @@ import { TECNICOSMOCK } from './../../../../utils/mocks/tecnicos.mock';
 export class AssociarComponent implements OnInit {
 
   tecnicos: TecnicoModel[] = TECNICOSMOCK;
+  atendimentos: AtendimentoModel[] = [];
 
-  constructor() {}
+  options: NgbModalOptions = {
+    size: 'lg'
+  };
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {}
 
-  add(tecnico) {
-    console.log(tecnico)
+  openModal(tecnico) {
+    const modalRef = this.modalService.open(AtendimentosDisponiveisComponent, this.options);
+    modalRef.componentInstance.tecnicoSelecionado = tecnico;
+    modalRef.result.then(result => this.atendimentos = result);
   }
 
 

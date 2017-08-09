d64aac02f992eecee33ab993c6f56187b9aaa6a2
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-08-09 17:53:57 -0300

    finished the method to remove atendimentos

--- src/app/pages/atendimentos/components/associar/associar.component.ts
+++ src/app/pages/atendimentos/components/associar/associar.component.ts
@@ -14,7 +14,6 @@ import { AtendimentosDisponiveisComponent } from './atendimentos-disponiveis/ate
 export class AssociarComponent implements OnInit {
   tecnicos: TecnicoModel[] = TECNICOSMOCK;
   atendimentos: AtendimentoModel[] = [];
-  tecnico: any;
 
   options: NgbModalOptions = {
     size: 'lg',
@@ -26,7 +25,9 @@ export class AssociarComponent implements OnInit {
 
   abrirModal(tecnicoSelecionado) {
 
-    const modalRef = this.modalService.open(AtendimentosDisponiveisComponent, this.options);
+    const modalRef = this.modalService
+                    .open(AtendimentosDisponiveisComponent, this.options);
+
     modalRef.componentInstance.tecnicoSelecionado = tecnicoSelecionado;
 
     modalRef.result.then(resultadoDaModal => {
@@ -45,9 +46,37 @@ export class AssociarComponent implements OnInit {
         });
     });
   }
+  
+  removerAtendimento(atendimento, tecnico) { 
+    /*
+        Verifica se o técnico do atendimento
+        é o mesmo que o técnico em que o usuário pretende remover
 
-  removerAtendimento(atendimento) {
-     const remover = this.tecnicos.map(elemento => elemento.atendimentos.splice(atendimento, 1));
-     console.log(remover);
+        Se for o mesmo, é retornado o valor.
+    */
+    const verificaAtdClicadoTecnicoIsIgual = 
+        atendimento.tecnico.find((nome) => nome === tecnico);
+        
+    /*
+        Verifica se o técnico do atendimento armazenado
+        é o mesmo que o técnico em que o usuário pretende remover
+    */
+     const verificaAtdArmazenadoTecnicoIsIgual = 
+        this.tecnicos.find((tecnicoProcurado) => 
+                    tecnicoProcurado.nome === verificaAtdClicadoTecnicoIsIgual);
+  
+    /*
+      Executando um laço no array de atendimentos do técnico obtendo
+      o índice do atendimento a ser removido
+
+    */
+      for (const i in verificaAtdArmazenadoTecnicoIsIgual.atendimentos) {
+        if (verificaAtdArmazenadoTecnicoIsIgual.atendimentos[i] === atendimento) {
+          
+          verificaAtdArmazenadoTecnicoIsIgual.atendimentos
+          .splice(verificaAtdArmazenadoTecnicoIsIgual.atendimentos.indexOf(atendimento), 1);
+        }
+      }
+    }
   }
-}
+

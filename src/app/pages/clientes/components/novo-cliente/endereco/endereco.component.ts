fbb4038c342ad47d99e7eacc4e732f9254a5c2f3
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-08-11 15:27:56 -0300

    add reactive forms to the endereco component in clientes pages with the purpose use cep service

--- src/app/pages/clientes/components/novo-cliente/endereco/endereco.component.ts
+++ src/app/pages/clientes/components/novo-cliente/endereco/endereco.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { AddressInfo } from './../../../../../models/addressInfo';
+import { CepService } from './../../../../../shared/services/cep-service/cep.service';
 
 @Component({
   selector: 'app-endereco-cliente',
@@ -6,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnderecoComponent implements OnInit {
 
-  constructor() { }
+  formEndereco: FormGroup;
+ 
+  constructor(private _cepService: CepService, private _fb: FormBuilder) {}
 
   ngOnInit() {
+    this.formEndereco = this._fb.group({
+      cep: ['', [Validators.required]],
+        rua: ['', [Validators.required]],
+        bairro: ['', [Validators.required]],
+        numero: ['', [Validators.required]],
+        cidade: ['', [Validators.required]],
+        complemento: ['', [Validators.required]],
+        uf: ['', [Validators.required]],
+    });
+  }
+
+  buscaPorCep(cep: string) {
+    this._cepService.obterInfoEndereco(cep).subscribe((data: AddressInfo) => {
+        this.formEndereco.get('cidade').patchValue(data.localidade);
+        this.formEndereco.get('complemento').patchValue(data.complemento);
+        this.formEndereco.get('uf').patchValue(data.uf);
+        this.formEndereco.get('rua').patchValue(data.logradouro);
+        this.formEndereco.get('bairro').patchValue(data.bairro);
+    });
+
+
   }
 
 }

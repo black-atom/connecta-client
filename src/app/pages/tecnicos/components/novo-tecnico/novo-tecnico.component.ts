82ffcd3f9890fef5c2e100167135b2e9acc763a1
alexandrejuk <ale_santos.soares@hotmail.com>
2017-08-11 16:57:19 -0300

    criação do formulário para salvar um novo técnico

--- src/app/pages/tecnicos/components/novo-tecnico/novo-tecnico.component.ts
+++ src/app/pages/tecnicos/components/novo-tecnico/novo-tecnico.component.ts
@@ -11,29 +11,43 @@ import { CepService } from './../../../../shared/services/cep-service/cep.servic
 })
 export class NovoTecnicoComponent implements OnInit {
 
-  formEndereco: FormGroup;
-  
+  formTecnico: FormGroup;
+
    constructor(private _cepService: CepService, private _fb: FormBuilder) {}
- 
+
    ngOnInit() {
-     this.formEndereco = this._fb.group({
-       cep: ['', [Validators.required]],
-        rua: ['', [Validators.required]],
-        bairro: ['', [Validators.required]],
-        numero: ['', [Validators.required]],
-        cidade: ['', [Validators.required]],
-        complemento: ['', [Validators.required]],
-        uf: ['', [Validators.required]],
+     this.formTecnico = this._fb.group({
+          nome: ['', [Validators.required]],
+          rg: ['', [Validators.required]],
+          cpf: ['', [Validators.required]],
+          data_nasc: ['', [Validators.required]],
+          email: ['', [Validators.required]],
+          telefone: ['', [Validators.required]],
+          celular: ['', [Validators.required]],
+          observacao: [''],
+          cnh: ['', [Validators.required]],
+          validade_carteira: ['', [Validators.required]],
+          rua: ['', [Validators.required]],
+          numero: ['', [Validators.required]],
+          completmento: ['', [Validators.required]],
+          bairro: ['', [Validators.required]],
+          cidade: ['', [Validators.required]],
+          estado: ['', [Validators.required]],
+          cep: ['', [Validators.required]],
+          createdAt: ['', [Validators.required]],
+          updatedAt: ['', [Validators.required]],
+          atendimentos: this._fb.array([])
      });
    }
- 
+
    buscaPorCep(cep: string) {
      this._cepService.obterInfoEndereco(cep).subscribe((data: AddressInfo) => {
-         this.formEndereco.get('cidade').patchValue(data.localidade);
-         this.formEndereco.get('complemento').patchValue(data.complemento);
-         this.formEndereco.get('uf').patchValue(data.uf);
-         this.formEndereco.get('rua').patchValue(data.logradouro);
-         this.formEndereco.get('bairro').patchValue(data.bairro);
+         this.formTecnico.get('rua').patchValue(data.logradouro);
+         this.formTecnico.get('bairro').patchValue(data.bairro);
+         this.formTecnico.get('cidade').patchValue(data.localidade);
+         this.formTecnico.get('estado').patchValue(data.uf);
+
      });
   }
 }
+

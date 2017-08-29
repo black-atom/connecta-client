4755960febd15d2464f8fdc459938657c1f1ee2b
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-08-29 11:33:00 -0300

    components: continue implementation of modal atendimentos

--- src/app/pages/atendimentos/components/associar/atendimentos-disponiveis/atendimentos-disponiveis.component.ts
+++ src/app/pages/atendimentos/components/associar/atendimentos-disponiveis/atendimentos-disponiveis.component.ts
@@ -1,37 +1,93 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs/Rx';
 
 import { Funcionario } from './../../../../../models';
 import { Atendimento } from './../../../../../models';
 import { AtendimentoService } from './../../../../../shared/services';
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap/datepicker/datepicker.module';
 
 @Component({
   selector: 'app-atendimentos-disponiveis',
   templateUrl: './atendimentos-disponiveis.component.html',
   styleUrls: ['./atendimentos-disponiveis.component.scss']
 })
-export class AtendimentosDisponiveisComponent implements OnInit {
+export class AtendimentosDisponiveisComponent implements OnInit, OnDestroy {
 
   @Input() 
   funcionarioSelecionado: Funcionario;
 
+  public formModalAtendimentos: FormGroup;
+  private sub: Subscription;
   public atendimentos: Atendimento[];
-  public selecionados: any[] = [];
+  public selecionados: Atendimento[] = [];
   public atendimentoVinculado: Atendimento[] = [];
-
+  public campoData: NgbDateStruct;
  
-  constructor(public activeModal: NgbActiveModal,
-              private _atendimentoService: AtendimentoService) { }
+  constructor(public _activeModal: NgbActiveModal,
+              private _atendimentoService: AtendimentoService,
+              private _ngbDateParserFormatter: NgbDateParserFormatter,
+              private _fb: FormBuilder) { }
   
   ngOnInit() {
+    this.retornarTodosAtendimentos();
+    this.iniciarForm();
   }
 
-  buscarPorData(data) {
-    this._atendimentoService.retornarAtendimentoPorData(data)
-    .subscribe(res => this.atendimentos = res);
+  iniciarForm() {
+    this.formModalAtendimentos = this._fb.group({
+      busca_data: [''],
+      busca_razao_social: ['']
+    });
+  }
+
+  retornarTodosAtendimentos() {
+    this.sub = this._atendimentoService
+                   .retornarTodos()
+                   .subscribe(res => this.atendimentos = res);
+
+    if (this.campoData !== undefined || null) {
+        this.campoData = null;
+    }
+  }
+
+  buscarPorData() {
+    const data = this._ngbDateParserFormatter.format(this.campoData);
+  
+    this.sub = this._atendimentoService
+                   .retornarAtendimentoPorData(data)
+                   .subscribe(res => this.atendimentos = res);
+
+    return data;
+  }
+
+  buscarPorNome(nome) {
+    this.atendimentos = this.atendimentos
+                  .filter((elemento) => {
+  
+    return elemento.razao_social
+                  .toLowerCase()
+                  .indexOf(nome.toLowerCase()) > -1;
+   });
+  }
+
+  cancelarBusca() {
+    if (this.buscarPorData()) {
+      return this.buscarPorData();
+    } else {
+      this.formModalAtendimentos.reset();
+      this.retornarTodosAtendimentos();
+    }
   }
 
   fecharModal() {
-    this.activeModal.dismiss();
+    this._activeModal.dismiss();
+  }
+
+  ngOnDestroy() {
+    if (this.sub) { 
+      this.sub.unsubscribe(); 
+    }
   }
 }

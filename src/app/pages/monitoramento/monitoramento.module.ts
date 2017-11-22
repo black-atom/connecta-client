4da362cd91c9d84e0717303bcf864f88c5c84125
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-11-22 17:53:59 -0200

    feature: start the improvement of monitoramento page

--- src/app/pages/monitoramento/monitoramento.module.ts
+++ src/app/pages/monitoramento/monitoramento.module.ts
@@ -4,11 +4,24 @@ import { NgaModule } from '../../theme/nga.module';
 
 import { routing } from './monitoramento.routing';
 import { MonitoramentoComponent } from './monitoramento.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  keyboardControl: true,
+  autoplay: 5000
+};
 
 @NgModule({
   imports: [
     CommonModule,
     NgaModule,
+    FlexLayoutModule,
+    SwiperModule.forRoot(SWIPER_CONFIG),
     routing
   ],
   declarations: [MonitoramentoComponent]

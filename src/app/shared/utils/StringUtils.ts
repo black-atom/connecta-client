76bc06a9a9c91cfbdcc168fe77dedc2a3026d5ab
Alexandre dos Santos Soares <alexandre.santos@localhost.localdomain>
2018-01-11 13:41:40 -0200

    create function propNameQuery

--- src/app/shared/utils/StringUtils.ts
+++ src/app/shared/utils/StringUtils.ts
@@ -1,7 +1,13 @@
 const removeMask = value => value.replace(/\D+/g, '');
 const removeMaskFromProp = prop => object => removeMask(object[prop]);
-
+const propNameQuery = filter => propName => {
+  if (filter[propName]) {
+    return { [propName]: filter[propName].value };
+  }
+  return { };
+};
 export {
   removeMask,
-  removeMaskFromProp
+  removeMaskFromProp,
+  propNameQuery
 };

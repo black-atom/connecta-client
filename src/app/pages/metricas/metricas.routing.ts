40ac96ec6bddd92d683c8b44b1e9e64e5f4b7a20
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-01-23 16:39:58 -0200

    feature: added route of component quilometragem. ~ ~ ~ ~ ~

--- src/app/pages/metricas/metricas.routing.ts
+++ src/app/pages/metricas/metricas.routing.ts
@@ -3,7 +3,7 @@ import { ModuleWithProviders } from '@angular/core';
 
 import { MetricasComponent } from './metricas.component';
 import { AtendimentosComponent } from './components/atendimentos/atendimentos.component';
-
+import { QuilometragemComponent } from './components/quilometragem/quilometragem.component';
 
 // noinspection TypeScriptValidateTypes
 export const routes: Routes = [
@@ -11,7 +11,8 @@ export const routes: Routes = [
     path: '',
     component: MetricasComponent,
     children: [
-        { path: 'atendimentos', component: AtendimentosComponent }
+        // { path: 'atendimentos', component: AtendimentosComponent },
+        { path: 'quilometragem', component: QuilometragemComponent }
      ]
   }
 ];

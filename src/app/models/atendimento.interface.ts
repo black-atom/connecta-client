da7eb0736189a7e1051204a5979d6cdf35cdc73f
alexandrejuk <ale_santos.soares@hotmail.com>
2018-02-19 11:24:10 -0300

    fix conflict

--- src/app/models/atendimento.interface.ts
+++ src/app/models/atendimento.interface.ts
@@ -75,11 +75,10 @@ interface Imagens {
 
 interface InteracaoTecnico {
   estado: string;
-  relatorio_tecnico: RelatorioRecnico;
+  relatorio_tecnico: Treinamento;
   retornar: Retorno;
   treinamento: Treinamento;
   remocao_relogio: RemocaoRelogio;
-  faturamento: Faturamento;
 }
 
 interface RelatorioRecnico {
@@ -109,6 +108,7 @@ interface RemocaoRelogio {
   bobina: boolean;
   fonte: boolean;
   pino: boolean;
+<<<<<<< HEAD
   impressora: boolean;
 }
 
@@ -120,4 +120,7 @@ interface Faturamento {
   quem_aprovou: string;
   valor: string;
   prazo_pagamento: string;
+=======
+  impresso: boolean;
+>>>>>>> fix-bug-update-associar: modify model of atendimentos
 }

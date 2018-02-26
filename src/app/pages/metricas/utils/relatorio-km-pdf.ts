4a47d03a863442395f3d496b0fa5e8496801ab0c
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-26 16:31:10 -0300

    feature: added formated data for print.
    Added formated data for print.

--- src/app/pages/metricas/utils/relatorio-km-pdf.ts
+++ src/app/pages/metricas/utils/relatorio-km-pdf.ts
@@ -1,26 +1,62 @@
 import * as jsPDF from 'jspdf';
+import { Monitoramento } from '../../../models/';
+import { LOGO_REALPONTO } from '../utils/logo-realponto';
+import { parseCnpjCpf, dataToString, horaToString } from '../utils/parse-dados';
 
-const doc = new jsPDF('landscape');
-let positionY: number = 10;
+let doc; 
 
 export const gerarRelatorio = funcionario => {
-  preencherRelatório(funcionario.monitoramentos);
-  doc.save(`${funcionario.nome}`);
+  doc = new jsPDF('landscape');
+  header(doc, funcionario);
+  preencherTabela(funcionario.monitoramentos);
+  doc.text(228, 195, 'Assinatura: ______________');
+  doc.save(
+    `${dataToString(funcionario.monitoramentos[0].data_hora_inicial_km)} - ${funcionario.nome}`
+  );
 };
 
-const createMonitoramento = async item => {
-  console.log(item);
-  await doc.text(15 , positionY, `monitoramento - > ${item.nome_razao_social}`);
-  positionY += 10;
-  console.log(positionY);  
+// tslint:disable-next-line:no-shadowed-variable
+const header = (doc, funcionario) => {
+  doc.addImage(LOGO_REALPONTO, 'PNG', 10, 3, 60, 20);
+  doc.setFontSize(18);
+  doc.text(115, 15, 'Controle de Visita Técnica ');
+  doc.text(115, 17, '_____________________');
+  doc.setFontSize(12);
+  doc.text(13, 32, `Técnico: ${funcionario.nome}`);
+  doc.text(132, 32, `Data: ${
+    dataToString(funcionario.monitoramentos[0].data_hora_inicial_km)
+  }`);
+  doc.text(233, 32, 'Veículo: ______________');
+  doc.setLineWidth(0.1);
+  doc.setDrawColor(197, 193, 207);
+  doc.line(300, 40, 0, 40);
 };
 
-const preencherRelatório = monitoramentos => {
-  // tslint:disable-next-line:prefer-for-of
-  for (let i = 0; i < monitoramentos.length; i++) {
-    if (monitoramentos[i]) {
-      positionY += 10;
-      doc.text(15 , positionY, `monitoramento - > ${monitoramentos[i].nome_razao_social}`);
+const preencherTabela = (monitoramentos: Monitoramento[]) => {
+  const columns = [
+    { title: 'Cliente', dataKey: 'nome_razao_social' },
+    // { title: 'CNPJ/CPF', dataKey: 'cnpj_cpf' }, 
+    { title: 'Tipo', dataKey: 'tipo' }, 
+    { title: 'Km Início', dataKey: 'km_inicial' }, 
+    { title: 'Km Final', dataKey: 'km_final' }, 
+    { title: 'Hora Km Início', dataKey: 'data_hora_inicial_km' }, 
+    { title: 'Hora Km Final', dataKey: 'data_hora_final_km' } 
+  ];  
+
+  const rows = monitoramentos.map(monitoramento => {
+    const parseDados = {
+      cnpj_cpf: parseCnpjCpf(monitoramento.cnpj_cpf),
+      data_hora_inicial_km: horaToString(monitoramento.data_hora_inicial_km),
+      data_hora_final_km: horaToString(monitoramento.data_hora_final_km)
+    };
+    return { ...monitoramento, ...parseDados };
+  });
+
+  doc.autoTable(columns, rows, {
+    margin: { top: 45 },
+    styles: {
+      fontSize: 10
     }
-  }
+  });
 };
+

4f9307a31503ec94598aa88f8acf143b00a04fc4
alexandrejuk <ale_santos.soares@hotmail.com>
2018-04-20 11:02:01 -0300

    route: add new route of navigation

--- src/app/pages/atendimentos/atendimentos.routing.ts
+++ src/app/pages/atendimentos/atendimentos.routing.ts
@@ -7,6 +7,7 @@ import { GerenciarComponent } from './components/gerenciar';
 import { DetalhesAtendimentoComponent } from './components/detalhes-atendimento';
 import { DadosInteracaoAppComponent } from './components/dados-interacao-app/dados-interacao-app.component';
 import { FormCanDeactivateGuard } from './../../shared/guards/form-deactivate.guard';
+import { AtendimentosConcluidosComponent } from './components/atendimentos-concluidos/atendimentos-concluidos.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,7 @@ const routes: Routes = [
       { path: 'novo', component: NovoAtendimentoComponent,  canDeactivate: [FormCanDeactivateGuard] },
       { path: 'associar', component: AssociarComponent },
       { path: 'gerenciar', component: GerenciarComponent },
+      { path: 'gerenciar-concluidos', component: AtendimentosConcluidosComponent },
       { path: 'detalhes/:id', component: DetalhesAtendimentoComponent,  canDeactivate: [FormCanDeactivateGuard] },
       { path: 'dados-app/:id', component: DadosInteracaoAppComponent },
     ]

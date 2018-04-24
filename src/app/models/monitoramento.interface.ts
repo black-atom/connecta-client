c1ef5ad7d052d0f6842364332524de70d0603bec
alexandrejuk <ale_santos.soares@hotmail.com>
2018-04-24 17:39:39 -0300

    monitoring: refactor monitoring page

--- src/app/models/monitoramento.interface.ts
+++ src/app/models/monitoramento.interface.ts
@@ -1,17 +1,60 @@
-export interface Monitoramento {
-    _id: string;
-    km_inicial: number;
-    km_final: number;
-    data_hora_inicial_km: Date;
-    data_hora_final_km: Date;
-    data_hora_inicial_virgente_local: Date;
-    data_hora_final_virgente_local: Date;
-    tipo: string;
-    id_funcionario: string;
-    id_atendimento: string;
-    duracao_evento?: string;
-    tempo_deslocamento?: string;
-    total_km_deslocamento?: number;
-    nome_razao_social?: string;
-    cnpj_cpf?: string;
+import { Atendimento } from './atendimento.interface';
+
+export enum AtividadeTipo {
+  almoco = 'almoco',
+  atendimento = 'atendimento',
+  deslocamento_empresa = 'deslocamento_empresa',
+  abastecimento = 'abastecimento',
+  outros = 'outros'
 }
+
+export enum MonitoramentoStatuses {
+  pendente = 'PENDENTE',
+  pauseAtividade = 'PAUSE_ATIVIDADE',
+  inicioAtividade = 'INICIO_ATIVIDADE',
+  fimAtividade = 'FIM_ATIVIDADE',
+  inicioDeslocamento = 'INICIO_DESLOCAMENTO',
+  criarAtividade = 'CRIAR_ATIVIDADE',
+  fimDeslocamento = 'FIM_DESLOCAMENTO',
+  cancelaAtividade = 'CANCELA_ATIVIDADE'
+}
+
+export interface Monitoramento {
+  status: MonitoramentoStatuses;
+  date: Date;
+  motivo?: string;
+}
+
+export interface Atividade {
+  _id?: string;
+  descricao: string;
+  atividade_id: string;
+  monitoramentos: Monitoramento[];
+  tipo: AtividadeTipo;
+  funcionario_id: string;
+  atendimento_id?: string;
+  atendimento?: Atendimento;
+  status: MonitoramentoStatuses;
+  synced: boolean;
+  localCreatedAt?: Date;
+  createdAt?: Date;
+}
+
+export interface MonitoramentoInfo {
+  funcionarioName: string;
+  totalPendentes: number;
+  totalPausados: number;
+  totalConcluidos: number;
+  atividadeAtual: Atividade;
+}
+
+export const statuses = {
+  PENDENTE: 'pendente',
+  PAUSE_ATIVIDADE: 'pausado',
+  INICIO_ATIVIDADE: 'execucao',
+  FIM_ATIVIDADE: 'concluido',
+  INICIO_DESLOCAMENTO: 'execucao',
+  FIM_DESLOCAMENTO: 'execucao',
+  CANCELA_ATIVIDADE: 'concluido',
+  CRIAR_ATIVIDADE: 'execucao'
+};
--- src/app/shared/services/atividade-service/atividade.service.ts
+++ src/app/shared/services/atividade-service/atividade.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 
 import { AuthHttp } from 'angular2-jwt';
 import { environment } from 'environments/environment';
@@ -12,8 +14,7 @@ import { ManipuladorErro } from './..';
 @Injectable()
 export class AtividadeService {
 
-  constructor(private _http: AuthHttp) {
-  }
+  constructor(private _http: AuthHttp) { }
 
   getAllAtividades() {
     return this._http
@@ -21,4 +22,12 @@ export class AtividadeService {
       .map(res => res.json())
       .catch(ManipuladorErro.lidaComErro);
   }
+
+  getAllAtividadesPorData(data) {
+    return this._http
+    .get(`${environment.API_ENDPOINT}/api/monitoramentos`, { params: { ...data } })
+      .map(res => res.json())
+      .catch(ManipuladorErro.lidaComErro);
+  }
+
 }

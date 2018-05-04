3fc7c2ebf0ee4dbcea260cdf047def4decff3a85
alexandrejuk <ale_santos.soares@hotmail.com>
2018-05-04 11:00:33 -0300

    fix-bug: of screen on details

--- src/app/pages/atendimentos/components/dados-interacao-app/dados-interacao-app.component.ts
+++ src/app/pages/atendimentos/components/dados-interacao-app/dados-interacao-app.component.ts
@@ -31,77 +31,8 @@ export class DadosInteracaoAppComponent implements OnInit {
 
 
   print(atendimento): void {
-    let printContents, popupWin;
-    printContents = document.getElementById('print-section').innerHTML;
-    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
-    popupWin.document.open();
-    popupWin.document.write(`
-      <html>
-        <head>
-          <title>${atendimento.cliente.nome_razao_social} - ${atendimento.cliente.cnpj_cpf}</title>
-          <style>
-
-            header nav, footer {
-              display: none;
-            }
-
-            img {
-              height: 80px;
-              width: auto;
-            }
-            @page {
-              margin: 0.3cm;
-            }
-            body {
-                background: #FFF;
-                color: #000;
-                font-family: Georgia, serif;
-                line-height: 1;
-            }
-
-            p {
-              margin: 0 0 1000px;
-            }
-
-            h4 {
-              font-size:1.1em;
-              font-weight: normal;
-              margin: 2em 0 0.5em;
-            }
-            .assinatura {
-              height: 120px;
-              width: auto;
-            }
-            .no-print {
-              display:none;
-            }
-
-          </style>
-          <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossorigin="anonymous">
-          <script
-          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-           crossorigin="anonymous"></script>
-          <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-          crossorigin="anonymous"></script>
-          <script
-          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-          crossorigin="anonymous"></script>
-        </head>
-       <body onload="window.print();window.close()">
-         ${printContents}
-       </body>
-      </html>`
-    );
-    popupWin.document.close();
-}
+    window.print();
+  }
 
   obterIdAtendimento() {
     this._activatedRoute.params.subscribe(params => this.id = params['id']);

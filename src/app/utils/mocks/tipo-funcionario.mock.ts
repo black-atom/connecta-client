01f6636033820d90b3732c226c2c7994917da02e
alexandrejuk <ale_santos.soares@hotmail.com>
2018-08-01 08:35:46 -0300

    fix positon of type staff

--- src/app/utils/mocks/tipo-funcionario.mock.ts
+++ src/app/utils/mocks/tipo-funcionario.mock.ts
@@ -3,7 +3,7 @@ import { Funcionario } from '../../models';
 export const TIPOFUNCIONARIOMOCK: string[] = [
     'administrador',
     'contrato',
-    'tecnica',
     'tecnico',
+    'tecnica',
     'suporte'
 ];
